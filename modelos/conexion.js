89d789cc981248969d73db38c4caf61e337e38e0
walter suazo <walter_suazo2002@hotmail.com>
2016-08-04 11:02:45 -0600

    modificar conexion

--- modelos/conexion.js
+++ modelos/conexion.js
@@ -4,14 +4,26 @@
 
 var mysql = require('mysql');
 
-
 ch = mysql.createPool({
-  host:'131.161.55.130',
-  user:'root',
-  password:'m4x3ll.gps!',
-  database:'Positions',
+  host:'honduras.ca6zya7z3wxn.us-west-2.rds.amazonaws.com',
+  user:'Master',
+  password:'H0ndu.gps!',
+  database:'Gateway1',
   dateStrings:'date',
   waitForConnections:false,
   connectionLimit:100
 });
 exports.ch=ch;
+
+
+
+oldch = mysql.createPool({
+  host:'131.161.55.130',
+  user:'root',
+  password:'m4x3ll.gps!',
+  database:'dev',
+  dateStrings:'date',
+  waitForConnections:false,
+  connectionLimit:100
+});
+exports.oldch=oldch;
