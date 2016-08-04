1b17b2d3787792aea4985370efbb96983a402cad
walter suazo <walter_suazo2002@hotmail.com>
2016-08-04 11:03:09 -0600

    Agregar modelo de Unidades
    Obtener el imei del gps a partir del unit id

---
+++ modelos/Unidades.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Walter Suazo on 19/01/2016.
+ */
+var pool = require('./conexion').oldch;
+
+unidadesModel= {};
+
+
+
+unidadesModel.getImei = function(id,callback)
+{
+  pool.getConnection(function(err,con) {
+   if(con){
+     query= "select imei from dev.DeviceNAME where UNITID=substr("+id+",7,4)";
+     con.query(query,function(err,row){
+       con.release();
+       if(err)
+       {
+         console.log(err);
+          callback(err,null)
+       }else
+       {
+         console.log(row);
+         callback(null,row)
+       }
+     });
+   }
+  });
+};
+
+
+module.exports= unidadesModel;
